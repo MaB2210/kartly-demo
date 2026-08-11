eb237149db30d83d27fc33b419ec1023417db27b
MaB2210 <connect.mannbhatt@gmail.com>
2026-08-11 11:54:41 -0400

    chore: initial Spring Boot setup with PostgreSQL connection via Docker

---
+++ src/main/java/kartly_demo/KartlyDemoApplication.java
@@ -0,0 +1,13 @@
+package kartly_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartlyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartlyDemoApplication.class, args);
+	}
+
+}
